e1794a55703544b714633fd5fd075afa6d6448b6
Mark Donker <mark.donker@opencirclesolutions.nl>
2025-06-01 14:01:01 +0200

    Spring init

---
+++ src/main/java/com/pinkrocade/ThreeKidFamily/ThreeKidFamilyApplication.java
@@ -0,0 +1,13 @@
+package com.pinkrocade.ThreeKidFamily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreeKidFamilyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreeKidFamilyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pinkrocade/ThreeKidFamily/ThreeKidFamilyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinkrocade.ThreeKidFamily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreeKidFamilyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
